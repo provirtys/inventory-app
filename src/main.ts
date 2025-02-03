95720c7c8a099a2b4e25a6a58e10998eae65732b
provirtys <provirtys@gmail.com>
2025-02-03 18:27:52 +0500

    add fonts, dependecies, etc.

--- src/main.ts
+++ src/main.ts
@@ -1,5 +1,11 @@
+import '@assets/style/index.scss';
 import { createApp } from 'vue'
-import './style.css'
 import App from './App.vue'
 
-createApp(App).mount('#app')
+
+const modules = import.meta.glob('./assets/icons/*.svg');
+Object.values(modules).forEach(async (el) => await el());
+
+const app = createApp(App);
+
+app.mount('#app');
--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,42 @@
-import { defineConfig } from 'vite'
-import vue from '@vitejs/plugin-vue'
+import { defineConfig } from 'vite';
+import vue from '@vitejs/plugin-vue';
+import createSvgSpritePlugin from 'vite-plugin-svg-sprite';
+import { fileURLToPath, URL } from 'url';
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [vue()],
-})
+  base:'./',
+  plugins: [
+    vue(),
+    createSvgSpritePlugin({
+      include: '**/icons/*.svg',
+      symbolId: '[name]'
+    })
+  ],
+  server: {
+    host: '192.168.1.4'
+  },
+  resolve: {
+    alias: {
+      '@': fileURLToPath(new URL('./src', import.meta.url)),
+      '@components': fileURLToPath(new URL('./src/components', import.meta.url)),
+      '@ui': fileURLToPath(new URL('./src/components/ui', import.meta.url)),
+      '@images': fileURLToPath(new URL('./src/assets/images', import.meta.url)),
+      '@stores': fileURLToPath(new URL('./src/stores', import.meta.url)),
+      '@pages': fileURLToPath(new URL('./src/pages', import.meta.url)),
+      '@layout': fileURLToPath(new URL('./src/layout', import.meta.url)),
+      '@router': fileURLToPath(new URL('./src/router', import.meta.url)),
+      '@assets': fileURLToPath(new URL('./src/assets', import.meta.url)),
+      '@helpers': fileURLToPath(new URL('./src/helpers', import.meta.url)),
+    },
+    extensions: ['.js', '.vue', '.scss', '.html', '.ts']
+  },
+  css: {
+    preprocessorOptions: {
+      scss: {
+        additionalData:
+          '@import "@assets/style/_mixins.scss"; @import "@assets/style/_variables.scss";'
+      }
+    }
+  }
+});
