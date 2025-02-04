f2f65be6cd891d3c2f4a98793f012aaab6afa3d4
provirtys <provirtys@gmail.com>
2025-02-05 00:25:21 +0500

    cange title and add gh-pages

--- src/helpers/image.ts
+++ src/helpers/image.ts
@@ -1,4 +1,5 @@
+const images = import.meta.glob('@assets/images/items/*.{png,jpg,jpeg,webp}', { eager: true });
+
 export const getImageUrl = (filename: string) => {
-  const url = new URL(`@assets/images/items`, import.meta.url).href
-  return `${url}/${filename}.png`;
-}
+  return (images[`/src/assets/images/items/${filename}.png`] as { default: string })?.default || '';
+};
